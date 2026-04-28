5de825353dc909ca9bae836ad70dc0c72b5db749
plantersclock <mattman861@gmail.com>
2026-04-28 16:23:01 +0700

    linked in added

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "./generic/components/Header";
 import Footer from "./generic/components/Footer";
 import NextTopLoader from "nextjs-toploader";
 import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script";
 
 const outfit = Outfit({
   variable: "--font-outfit",
@@ -32,6 +33,31 @@ export default function RootLayout({
         {children}
         <Footer />
         <Analytics />
+        {/* LinkedIn Insight Tag */}
+        <Script id="linkedin-partner" strategy="afterInteractive">
+          {`_linkedin_partner_id = "8644938";
+window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+window._linkedin_data_partner_ids.push(_linkedin_partner_id);`}
+        </Script>
+        <Script id="linkedin-insight" strategy="afterInteractive">
+          {`(function(l) {
+if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+window.lintrk.q=[]}
+var s = document.getElementsByTagName("script")[0];
+var b = document.createElement("script");
+b.type = "text/javascript";b.async = true;
+b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+s.parentNode.insertBefore(b, s);})(window.lintrk);`}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            alt=""
+            src="https://px.ads.linkedin.com/collect/?pid=8644938&fmt=gif"
+          />
+        </noscript>
       </body>
     </html>
   );
